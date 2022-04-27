3be4a8a8e4b0aa82254113e7961df2dd75e85adf
MoChebbo <mchebbo215@gmail.com>
2022-04-27 16:04:30 +0300

    PropertyAdd

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,9 @@
 function myFunction() {
-  // Declare variables
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("SearchInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("Table");
-  tr = table.getElementsByTagName("tr"); // Loop through all table rows, and hide those who don't match the search query
+  tr = table.getElementsByTagName("tr"); 
 
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
