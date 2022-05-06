606195988ff931fe36adf0ee52ed0caec2b7f425
alloush-22 <abbasalloush1@gmail.com>
2022-05-06 11:12:49 +0300

    contact update
    validate personal details

---
+++ Website/contact-page/contact.js
@@ -0,0 +1,62 @@
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()  
+
+//checkbox validation
+function validate() {
+  var valid = false;
+  if (document.getElementById("cb1").checked){
+    valid = true;
+  }
+  else if (document.getElementById("cb2").checked){
+    valid = true;
+  }
+  if (valid){
+    document.getElementById("checkvalidity").write("checked")
+  }
+  else{
+    document.getElementById("checkvalidity").write("please make sure you accept the terms!")
+  }
+
+}
+
+function verifyPassword() {  
+  var pw = document.getElementById("pswd").value;  
+  //check empty password field  
+  if(pw == "") {  
+     document.getElementById("message").innerHTML = "**Fill the password please!";  
+     return false;  
+  }  
+   
+ //minimum password length validation  
+  if(pw.length < 8) {  
+     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+     return false;  
+  }  
+  
+//maximum length of password validation  
+  if(pw.length > 15) {  
+     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+     return false;  
+  } else {  
+     alert("Password is correct");  
+  }  
+}  
